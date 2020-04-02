3ee2fea384d05fa616c805637abe3fdf9589fec4
kbeksss <kbeksss@outlook.com>
2020-04-02 22:09:46 +0600

    Added ability to send and receive messages

--- back/index.js
+++ back/index.js
@@ -17,12 +17,43 @@ const connections = {};
 const messages = [];
 
 app.ws('/messenger', function(ws, req) {
-    console.log('client connected');
     const id = nanoid();
-    connections[id] = ws;
 
+    console.log('client connected', id);
+    connections[id] = ws;
+    console.log('clients: ', Object.keys(connections).length);
+
+    ws.on('message', msg => {
+        console.log('message from ', id, ' : ', msg)
+        const parsed = JSON.parse(msg);
+
+        switch (parsed.type) {
+            case 'SEND_MESSAGE':
+                const receivedMessage = {
+                    text: parsed.text,
+                };
+                Object.keys(connections).forEach(c => {
+                    connections[c].send(JSON.stringify({
+                        type: 'RECEIVE_MESSAGE',
+                        ...receivedMessage
+                    }))
+                });
+                messages.push(receivedMessage);
+                if(messages.length > 15){
+                    messages.splice(0, 1);
+                }
+                break;
+            default:
+                break;
+        }
+    });
 
     ws.on('close', msg => {
-        console.log('disconnected', connections[id]);
+        console.log('disconnected', id);
+        delete connections[id]
     })
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port: ', port);
 });
